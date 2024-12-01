8c532a967cff0805ee6c580ae700f5f5bf2104be
fai2an <mohammedfaizangm@gmail.com>
2024-12-01 16:38:00 +0100

    style: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Root HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // Source files
+  ],
+  theme: {
+    extend: {}, // Add custom styles here
+  },
+  plugins: [],
+};
